b0557f5b2e44bf6a79ab3f958641793c9c0e9f63
chaubv <minhchau.utc@gmail.com>
2024-03-04 15:02:32 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-TAG=`git rev-parse --short HEAD`
-echo $TAG
-docker build -t project-soc:$TAG .
+TAG_VERSION=`git rev-parse --short HEAD`
+echo $TAG_VERSION
+sed -i s/tag/$TAG_VERSION/g docker-compose.yaml
+docker build -t project-soc:$TAG_VERSION .
