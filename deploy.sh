594aeb3d4f0dab1a40008c8e81a3bd9a05f30583
chaubv <minhchau.utc@gmail.com>
2024-03-05 20:12:17 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,81 +1,90 @@
 #!/bin/bash
 remote_user_dev=ubuntu
-remote_host_dev=192.168.1.10
-remote_password_dev=xxxxx
+remote_host_dev=192.168.3.5
+remote_password_dev=imip@1234
 remote_user_staging=ubuntu
-remote_host_staging=192.168.1.11
-remote_password_staging=xxxxxx
+remote_host_staging=192.168.3.11
+remote_password_staging=imip@1234
 remote_user_prod=ubuntu
-remote_host_prod=192.168.1.12
-remote_password_prod=xxxxx
+remote_host_prod=192.168.3.14
+remote_password_prod=imip@1234
 ORIGIN_DIRECTORY="project-soc"
 GIT_URI="https://github.com/chaubv/project-soc.git"
-
+env_dev=dev
+env_staging=staging
+env_prod=prod
+echo "environment: $1"
+echo "version: $2"
 # Answer choose environment
 echo
-echo -n "Do you want to build and deploy on dev,staging or prod environment?. (dev/staging/prod) "
-read -r answer
-
-if [ "$answer" = "dev" ] ; then
+if [ "$env_dev" = "$1" ] ; then
     echo
     echo "dev environment"
     ENVIRONMENT="dev"
-    sshpass -p "$remote_password_dev" ssh -T -o StrictHostKeyChecking=no "$remote_user_dev"@"$remote_host_dev" << EOF
+    ssh $remote_user_dev@$remote_host_dev << EOF
     rm -rf $ORIGIN_DIRECTORY
     git clone $GIT_URI --branch dev
-    cd $ORIGIN_DIRECTORY && chmod 755 run.sh && ./run.sh
+    cd $ORIGIN_DIRECTORY && sed -i s/tag/$2/g docker-compose.yaml && docker build -t project-soc:$2 .
 EOF
-# Deploy to dev environment and version will tag commit hash ID
+# Deploy to dev environment
     echo -n "Do you want to continue for deploy to dev environment? (y/n)?"
     read -r answer
     if [ "$answer" != "${answer#[Yy]}" ] ; then
-       echo "Yes! You starting deploy"
-       sshpass -p "$remote_password_dev" ssh -T -o StrictHostKeyChecking=no "$remote_user_dev"@"$remote_host_dev" "cd $ORIGIN_DIRECTORY && docker-compose up -d --build"
-    else
+       echo "Yes! you start deploy"
+ #      sshpass -p "$remote_password_dev" ssh -T -o StrictHostKeyChecking=no "$remote_user_dev"@"$remote_host_dev" "cd $ORIGIN_DIRECTORY && docker-compose up -d --build"
+       ssh $remote_user_dev@$remote_host_dev << EOF
+       cd $ORIGIN_DIRECTORY && docker-compose up -d --build
+EOF
+   else
       echo "Stop deployment"
       exit
     fi
     
-elif [ "$answer" = "staging" ] ; then
+elif [ "$env_staging" = "$1" ] ; then
     echo
     echo "staging environment"
     ENVIRONMENT="staging"
-    sshpass -p "$remote_password_staging" ssh -T -o StrictHostKeyChecking=no "$remote_user_staging"@"$remote_host_staging" << EOF
+    ssh $remote_user_staging@$remote_host_staging << EOF
     rm -rf $ORIGIN_DIRECTORY
     git clone $GIT_URI --branch staging
-    cd $ORIGIN_DIRECTORY && chmod 755 run.sh && ./run.sh
+    cd $ORIGIN_DIRECTORY && sed -i s/tag/$2/g docker-compose.yaml && docker build -t project-soc:$2 .
 EOF
-# Deploy to staging environment and version will tag commit hash ID
+# Deploy to staging environment
     echo -n "Do you want to continue for deploy to staging environment? (y/n)?"
     read -r answer
     if [ "$answer" != "${answer#[Yy]}" ] ; then
-       echo "Yes! You starting deploy"
-       sshpass -p "$remote_password_staging" ssh -T -o StrictHostKeyChecking=no "$remote_user_staging"@"$remote_host_staging" "cd $ORIGIN_DIRECTORY && docker-compose up -d --build"
+       echo "Yes! You start deploy"
+       ssh $remote_user_staging@$remote_host_staging << EOF
+       cd $ORIGIN_DIRECTORY && docker-compose up -d --build
+EOF
     else
       echo "Stop deployment"
       ls -la
       exit
     fi
 
-elif [ "$answer" = "prod" ] ; then
+elif [ "$env_prod" = "$1" ] ; then
     echo
     echo "prod environment"
     ENVIRONMENT="prod"
-    sshpass -p "$remote_password_prod" ssh -T -o StrictHostKeyChecking=no "$remote_user_prod"@"$remote_host_prod" << EOF
+    ssh "$remote_user_prod"@"$remote_host_prod" << EOF
     rm -rf $ORIGIN_DIRECTORY
     git clone $GIT_URI --branch main
-    cd $ORIGIN_DIRECTORY && chmod 755 run.sh && ./run.sh
+    cd $ORIGIN_DIRECTORY && sed -i s/tag/$2/g docker-compose.yaml && docker build -t project-soc:$2 .
 EOF
-# Deploy to prod environment and version will tag commit hash ID
+# Deploy to prod environment
     echo -n "Do you want to continue for deploy to prod environment? (y/n)?"
     read -r answer
     if [ "$answer" != "${answer#[Yy]}" ] ; then
-       echo "Yes! You starting deploy"
-       sshpass -p "$remote_password_prod" ssh -T -o StrictHostKeyChecking=no "$remote_user_prod"@"$remote_host_prod" "cd $ORIGIN_DIRECTORY && docker-compose up -d --build"
+       echo "Yes! you start deploy"
+       ssh $remote_user_prod@$remote_host_prod << EOF
+       cd $ORIGIN_DIRECTORY && docker-compose up -d --build
+EOF
     else
       echo "Stop deployment"
       exit
     fi
+
 else
     echo
     echo "Bad selection"
